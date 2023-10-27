32410f4a3f534a316737ce734b734a418659981b
mxrcos999 <marcosfelipehd4@gmail.com>
2023-10-26 21:34:25 -0300

    Update

--- PicEnfermagem.Domain/Entities/Answer.cs
+++ PicEnfermagem.Domain/Entities/Answer.cs
@@ -4,13 +4,13 @@ namespace PicEnfermagem.Domain.Entities;
 
 public sealed class Answer : EntityCore
 {
-    public int AnswerTime { get; private set; } 
+    public double AnswerTime { get; private set; } 
     public int QuestionId { get; private set; }
     public double Punctuation {  get; private set; }
     public ApplicationUser? User { get; private set; }
     public string? UserId { get; set; }
 
-    internal Answer(int questionId, double punctuation, int answerTime)
+    internal Answer(int questionId, double punctuation, double answerTime)
     {
         QuestionId = questionId;
         Punctuation = punctuation;
--- PicEnfermagem.Domain/Factories/AnswerFactory.cs
+++ PicEnfermagem.Domain/Factories/AnswerFactory.cs
@@ -4,7 +4,7 @@ namespace PicEnfermagem.Domain.Factories;
 
 public static class AnswerFactory
 {
-    public static Answer Create(int questionId, double punctuation, int time)
+    public static Answer Create(int questionId, double punctuation, double time)
     {
         return new Answer(questionId, punctuation, time);  
     }
