dc842f84f2bc6b71fc6c169d80e6d7b010ade079
mxrcos999 <marcosfelipehd4@gmail.com>
2023-10-27 13:34:09 -0300

    update

---
+++ PicEnfermagem.Infraestrutura/Migrations/20231027003538_updatepropertyAnswer.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PicEnfermagem.Infraestrutura.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatepropertyAnswer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "AnswerTime",
+                table: "answer",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AnswerTime",
+                table: "answer",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+    }
+}
